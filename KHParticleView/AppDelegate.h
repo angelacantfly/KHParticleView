cf2523c8882da9dca27d713737b5b58ae0d63a96
Khanh Huy <lephukhanhhuy@gmail.com>
2013-08-11 16:41:46 +0700

    Initial Commit

---
+++ KHParticleView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  KHParticleView
+//
+//  Created by Khanh Huy on 8/11/13.
+//  Copyright (c) 2013 Khanh Huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ KHParticleView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KHParticleView
+//
+//  Created by Khanh Huy on 8/11/13.
+//  Copyright (c) 2013 Khanh Huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
